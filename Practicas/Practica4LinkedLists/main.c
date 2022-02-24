7cf7a2ebb596650b2c5290ebc01683d0b62d8385
Alfonso Casanova <alfonso.casanova.009@outlook.com>
2022-02-24 10:52:56 -0600

    Todo bem

---
+++ Practicas/Practica4LinkedLists/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+
+
+typedef struct {
+    int data;
+    struct Nodo *next;
+}Nodo;
+
+int main()
+{
+   
+    return 0;
+}
+
+
