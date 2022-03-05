5b13d73bedfa7312f9b90c5edcd5a654ae57ab0c
Alfonso Casanova <alfonso.casanova.009@outlook.com>
2022-03-04 19:16:36 -0600

    cambios mayores generales

--- Practicas/Practica5.1LinkedListsAdvanced/main.c
+++ Practicas/Practica5.1LinkedListsAdvanced/main.c
@@ -17,48 +17,5 @@ int main()
     
 
 
-    // ListaDoble * listaDobleDePuntos;
-    // ListaDoble * listaDobleDePuntos2;
-    // Punto * unPunto;
-
-    // inicializarPunto(&unPunto, 1, 2, 3);    
-    // inicializarListaDoble(&listaDobleDePuntos,unPunto);
-    // inicializarPunto(&unPunto, 1, 2, 3);    
-    // inicializarListaDoble(&listaDobleDePuntos2,unPunto);
-
-
-    // for (size_t i = 1; i < 50; i++)
-    // {
-        
-    //     inicializarPunto(&unPunto, i, i, i);    
-    //     agregarInicio(&listaDobleDePuntos,unPunto); 
-    //     inicializarPunto(&unPunto, i, i, i);    
-    //     agregarInicio(&listaDobleDePuntos2,unPunto); 
-        
-        
-    // }
-    
-
-    // // for (size_t i = 0; i < 32; i++)
-    // // {
-    // //     eliminarFinal(&listaDobleDePuntos);
-    // // }
-    
-    
-    
-    // toString(&listaDobleDePuntos, displayPunto);
-    // iterar(&listaDobleDePuntos, displayPunto);
-
-
-
-
     return 0;
 }
-
-// void miFuncionEjemplo(){
-//     string1 = (char*) malloc(16);
-//     strcpy(string1, "0123456789AB");
-//     string2 = realloc(string1, 64);
-//     printf("string1 valor: %p [%s]\n", string1, string1);
-//     printf("string2 valor: %p [%s]\n", string2, string2);
-// }
