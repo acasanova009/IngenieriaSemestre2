f9490688c658eecb2ca5dffe4280e180646073bb
Alfonso Casanova <alfonso.casanova.009@outlook.com>
2022-03-17 09:49:29 -0600

    FilaDoble Check

--- PracticasPro/main.c
+++ PracticasPro/main.c
@@ -1,8 +1,18 @@
-
+#include "filaDoble.h"
+#include "myString.h"
 
 
 int main(int argc, char const *argv[])
 {
+    FilaDoble *fila = newFilaDoble();
+    filaDoblePushTop(fila, "hola");
+    filaDoblePushTop(fila, "hola");
+    filaDoblePushTop(fila, "hola");
+    filaDoblePushTop(fila, "adios");
+
+    filaDobleIterarFromTop(fila, stringDisplay);
+
+    
     return 0;
 }
 
